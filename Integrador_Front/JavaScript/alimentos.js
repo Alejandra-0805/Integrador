bc39c6fa46dd1d277b06c73e1baa06793dcf3669
Alejandra-0805 <142358253+Alejandra-0805@users.noreply.github.com>
2025-12-04 19:44:58 -0600

    Delete Integrador_Front directory

--- Integrador_Front/JavaScript/alimentos.js
+++
@@ -1,8 +0,0 @@
-  const btnAgregar = document.querySelector(".btn-agregar");
-  btnAgregar.addEventListener("click", () => {
-    window.location.href = "agregarAlimento.html";
-  });
-
-document.querySelector('.activo2').addEventListener('click', function() {
-  alert("Generando reporte...");
-});
